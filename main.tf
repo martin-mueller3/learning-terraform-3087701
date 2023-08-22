4ca4630fe363266f5154592624091a8ec8de8e7b
Martin Mueller <martin.mueller3@zf.com>
2023-08-22 10:42:59 +0200

    new vpc using module

--- main.tf
+++ main.tf
@@ -14,15 +14,28 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]  
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true  
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [ module.blog.security_group_id ]
+  vpc_security_group_ids = [ module.blog_sg.security_group_id ]
 
   tags = {
     Name = "BobBobson"
@@ -34,7 +47,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog"
   
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
