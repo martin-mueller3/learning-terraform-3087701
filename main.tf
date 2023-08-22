ff0f17e4ad5d9898be664232070c9995d8df3e99
Martin Mueller <martin.mueller3@zf.com>
2023-08-22 11:08:46 +0200

    autoscaling replaces instance

--- main.tf
+++ main.tf
@@ -31,15 +31,20 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  vpc_security_group_ids = [ module.blog_sg.security_group_id ]
+  vpc_zone_identifier = module.blog.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [ module.blog_sg.security_group_id ]
 
-  tags = {
-    Name = "BobBobson"
-  }
+  image_id            = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
 }
 
 module "blog_alb" {
